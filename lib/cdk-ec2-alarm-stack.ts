e7a3a5634eb06b06322ff9c6a6e85b4c59cf0759
sfidahussain <sanfidahussain@gmail.com>
2022-10-18 13:48:47 -0700

    initial

--- lib/cdk-ec2-alarm-stack.ts
+++ lib/cdk-ec2-alarm-stack.ts
@@ -1,19 +1,74 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps, aws_sns_subscriptions, CfnParameter, aws_sns, aws_lambda, aws_events,
+  aws_events_targets, aws_iam } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as path from "path";
 
 export class CdkEc2AlarmStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const myTopic = new aws_sns.Topic(this, 'ec2-alarms');
 
-    const queue = new sqs.Queue(this, 'CdkEc2AlarmQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    myTopic.addSubscription(new aws_sns_subscriptions.EmailSubscription('sanfidahussain@gmail.com'));
+
+    const myRole = new aws_iam.Role(this, 'My Role', {
+      assumedBy: new aws_iam.ServicePrincipal('lambda.amazonaws.com'),
     });
 
-    const topic = new sns.Topic(this, 'CdkEc2AlarmTopic');
+    const fn = new aws_lambda.Function(this, 'LambdaFunction', {
+      runtime: aws_lambda.Runtime.PYTHON_3_8,
+      handler: 'cw_auto_alarms.lambda_handler',
+      role: myRole,
+      code: aws_lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
+      environment: {
+        "ALARM_CPU_CREDIT_BALANCE_LOW_THRESHOLD": "100",
+        "ALARM_CPU_HIGH_THRESHOLD": "75",
+        "ALARM_DISK_PERCENT_LOW_THRESHOLD": "20",
+        "ALARM_LAMBDA_ERROR_THRESHOLD": "0",
+        "ALARM_LAMBDA_THROTTLE_THRESHOLD": "0",
+        "ALARM_MEMORY_HIGH_THRESHOLD": "75",
+        "ALARM_TAG": "Create_Auto_Alarms",
+        "CLOUDWATCH_APPEND_DIMENSIONS": "InstanceId, ImageId, InstanceType",
+        "CLOUDWATCH_NAMESPACE": "CWAgent",
+        "CREATE_DEFAULT_ALARMS": 'true',
+        "DEFAULT_ALARM_SNS_TOPIC_ARN": myTopic.topicArn
+       }
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const ec2Rule = new aws_events.Rule(this, 'ec2Rule', {
+      eventPattern: {
+        source: ["aws.ec2"],
+        detailType: ["EC2 Instance State-change Notification"],
+        detail: {
+          "state": [
+            "running",
+            "terminated"
+          ]
+        }
+      },
+    });
+
+    const lambdaRule = new aws_events.Rule(this, 'lambdaRule', {
+      eventPattern: {
+          "source": [
+          "aws.lambda"
+        ],
+            detailType: [
+          "AWS API Call via CloudTrail"
+        ],
+            detail: {
+          "eventSource": [
+            "lambda.amazonaws.com"
+          ],
+              eventName: [
+            "TagResource20170331v2",
+            "DeleteFunction20150331"
+          ]
+        }
+      }
+    });
+
+
+    ec2Rule.addTarget(new aws_events_targets.LambdaFunction(fn));
+    lambdaRule.addTarget(new aws_events_targets.LambdaFunction(fn))
   }
 }
